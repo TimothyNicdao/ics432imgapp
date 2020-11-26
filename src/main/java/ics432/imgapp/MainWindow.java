d3759da4197252e2449ee2dd562cc24ee3a73fd9
angeli-amascual <aamascua@hawaii.edu>
2020-11-25 19:35:32 -1000

    Update Processor Thread

--- src/main/java/ics432/imgapp/MainWindow.java
+++ src/main/java/ics432/imgapp/MainWindow.java
@@ -368,6 +368,7 @@ class MainWindow {
 
                 if (wu.end == true) {
                     try {
+                        wu.processImage();
                         this.mw.toWrite.put(wu);
                     } catch (InterruptedException e) {
                         //  We're canceled
